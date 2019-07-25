f21cb4a218ff1154d114477096e2663df6e2305b
Sarfaroj <debasmita.techexactly@gmail.com>
2019-07-26 00:06:58 +0530

    make it better

--- src/app/user/share/service.service.ts
+++ src/app/user/share/service.service.ts
@@ -6,16 +6,19 @@ import { HttpClient } from "@angular/common/http";
   providedIn: "root"
 })
 export class AuthService {
+
+private baseUrl ="https://m-mashi.herokuapp.com/";
+  
+    private registerUrl = this.baseUrl+ "api/v1/user/register/";
+    private loginUrl = this.baseUrl+ "api/v1/user/login/"
+
   constructor(private http: HttpClient) {}
 
   public register(userData: any): Observable<any> {
-    return this.http.post(
-      "http://localhost:3001/api/v1/user/register",
-      userData
-    );
+    return this.http.post(this.registerUrl, userData);
   }
 
   public login(userData: any): Observable<any> {
-    return this.http.post("http://localhost:3001/api/v1/user/login", userData);
+    return this.http.post(this.loginUrl, userData);
   }
 }
